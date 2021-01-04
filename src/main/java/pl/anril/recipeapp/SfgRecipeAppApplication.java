16465e01a4661b4ace9c55f92bd26134ea05866b
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-01-04 19:42:51 +0100

    created project

---
+++ src/main/java/pl/anril/recipeapp/SfgRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package pl.anril.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRecipeAppApplication.class, args);
+    }
+
+}
