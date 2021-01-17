456316a61a7a93a4049683ed36efd5d2e36ab9f7
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-01-17 21:30:44 +0100

    Enumerated:

--- src/main/java/pl/anril/recipeapp/domain/Recipe.java
+++ src/main/java/pl/anril/recipeapp/domain/Recipe.java
@@ -22,6 +22,7 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
     @Lob
     private Byte[] image;
