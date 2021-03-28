0aec23e0fa70e21412893cb148893ebbb3893b8e
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-03-28 21:07:43 +0200

    after assignment

--- src/test/java/pl/anril/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/pl/anril/recipeapp/controllers/IndexControllerTest.java
@@ -2,17 +2,21 @@ package pl.anril.recipeapp.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import pl.anril.recipeapp.domain.Recipe;
 import pl.anril.recipeapp.service.RecipeService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
     IndexController indexController;
@@ -28,8 +32,21 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setDescription("test");
+        recipes.add(recipe);
+
+        when(recipeService.findAll()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         assertThat(indexController.getIndexPage(model), equalTo("index"));
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         verify(recipeService, times(1)).findAll();
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertThat(setInController.size(), equalTo(2));
     }
 }
