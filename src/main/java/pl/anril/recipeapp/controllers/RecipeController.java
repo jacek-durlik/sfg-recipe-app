0a38ccb766cfb9de423723ceb6f4ed3eb7eb1bd5
Jacek Durlik <jacek.jan.durlik@gmail.com>
2021-05-02 05:48:47 +0200

    after recipe update

--- src/main/java/pl/anril/recipeapp/controllers/RecipeController.java
+++ src/main/java/pl/anril/recipeapp/controllers/RecipeController.java
@@ -3,8 +3,11 @@ package pl.anril.recipeapp.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.anril.recipeapp.domain.Recipe;
 import pl.anril.recipeapp.repositories.RecipeRepository;
 
 @RequestMapping("/recipe")
@@ -13,9 +16,29 @@ import pl.anril.recipeapp.repositories.RecipeRepository;
 public class RecipeController {
     private final RecipeRepository recipeRepository;
 
-    @RequestMapping("/show/{recipeId}")
+    @RequestMapping("/{recipeId}/show")
     public String showRecipe(Model model, @PathVariable String recipeId) {
         model.addAttribute("recipe", recipeRepository.findById(Long.valueOf(recipeId)).orElse(null));
         return "recipe/show";
+    }
+
+    @RequestMapping("/{recipeId}/update")
+    public String updateRecipe(Model model, @PathVariable String recipeId) {
+        model.addAttribute("recipe", recipeRepository.findById(Long.valueOf(recipeId)).orElse(null));
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new Recipe());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("/")
+    public String saveOrUpdateRecipe(@ModelAttribute Recipe recipe) {
+        Recipe savedRecipe = recipeRepository.save(recipe);
+
+        return "redirect:/recipe/" + savedRecipe.getId() + "/show";
     }
 }
